afc3a9d2e57f89cdc1f80bbb2348e74633a50717
bttt8888444 <bttt8888444@gmail.com>
2026-02-09 04:26:11 +0300

    Initial Commit

---
+++ CaptainApp/CaptainAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptainAppApp.swift
+//  CaptainApp
+//
+//  Created by user on 09/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct CaptainAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
